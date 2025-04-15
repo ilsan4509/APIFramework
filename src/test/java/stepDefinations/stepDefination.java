e0a9a960e68ba888678d5060813c2d30682e8115
Seonggon <ilsan4509@naver.com>
2025-04-15 22:33:12 +0100

    Add reusable response specification and implement request handling in step definition for Add Place API

--- src/test/java/stepDefinations/stepDefination.java
+++ src/test/java/stepDefinations/stepDefination.java
@@ -2,68 +2,40 @@ package stepDefinations;
 
 import static io.restassured.RestAssured.given;
 
-import java.util.ArrayList;
-import java.util.List;
 import static org.junit.Assert.*;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import pojo.AddPlace;
-import pojo.Location;
+import resources.TestDataBuild;
+import resources.Utils;
 
-public class stepDefination {
+public class stepDefination extends Utils {
 	
 	RequestSpecification res;
 	ResponseSpecification resspec;
 	Response response;
+	TestDataBuild data = new TestDataBuild();
 	
 	@Given("Add Place Payload")
 	public void add_Place_Payload() {
-		
-		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
-		AddPlace p = new AddPlace();
-		p.setAccuracy(50);
-		p.setAddress("29, side layout, cohen 09");
-		p.setLanguage("French-IN");
-		p.setPhone_number("(+91) 983 893 3937");
-		p.setWebsite("https://rahulshettyacademy.com");
-		p.setName("Frontline house");
-		List<String> myList = new ArrayList<String>();
-		myList.add("shoe park");
-		myList.add("shop");
-		
-		p.setTypes(myList);
-		Location l = new Location();
-		l.setLat(38.383494);
-		l.setLng(33.427362);
-		p.setLocation(l); // Set Location object in AddPlace object
-		
-		// Build a reusable request specification (재사용 가능한 요청 사양 생성)
-		RequestSpecification req = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", "qaclick123")
-		.setContentType(ContentType.JSON).build();
-		
-		// Build a reusable response specification (재사용 가능한 응답 사양 생성)
+		// Build a reusable response specification
 		resspec = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 		
-		// Prepare and send the request using spec builder (사양 객체를 사용하여 요청 준비 및 전송)
-		res = given().spec(req)
-		.body(p); // Attach POJO as request body
+		// Prepare and send the request using spec builder
+		res = given().spec(requestSpecification())
+		.body(data.addPlacePayLoad()); // Attach POJO as request body
 		
 	}
 	
 	@When("user calls {string} with post http request")
 	public void user_calls_with_http_request(String string) {
-		// Send POST request and apply response spec for validation
 		response = res.when().post("/maps/api/place/add/json")
 			.then().spec(resspec).extract().response();
 	}
